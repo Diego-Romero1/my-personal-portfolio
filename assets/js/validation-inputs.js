fd7a77cd819f624d7e2db0fc6096d884e176a301
Diego Romero <redgo110.krextro@gmail.com>
2023-03-26 13:21:15 -0300

    inputs validations working

--- assets/js/validation-inputs.js
+++ assets/js/validation-inputs.js
@@ -46,11 +46,8 @@ const mensajesDeError = {
     let mensaje = "";
     tipoDeErrores.forEach((error) => {
       if (input.validity[error]) {
-        console.log(tipoDeInput, error);
-        console.log(input.validity[error]);
-        console.log(mensajesDeError[tipoDeInput][error]);
         mensaje = mensajesDeError[tipoDeInput][error];
       }
     });
-    return mensaje;
+    return mensaje
   }
